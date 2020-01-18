3901ec41f9021eca9c93beaa886d2f58bc5dd00f
Darryl <darrylmangibin@gmail.com>
2020-01-18 22:46:49 +0800

    mongodb config

--- server.js
+++ server.js
@@ -1,10 +1,21 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
 
 app.get('/', (req, res) => {
   res.send('test')
 });
+
+// MongoDB COnfiguration
+const db = require('./config/keys').mongoURI;
+mongoose
+  .connect(db, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .then(() => console.log('MongoDB Connected'))
+  .catch(() => console.log('Connection failed'))
 
 // PORT
 const port = process.env.PORT || 5000;
